da0e429c4ca978e0b38b0cd04e9fcc4e17c2d772
ilkinimanov <coderilkin.03@gmail.com>
2023-06-07 16:41:56 +0400

    Connect to database according to NODE_ENV - Mongoose ORM included to project - Project is connecting to database according to NODE_ENV

---
+++ src/db/db.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+exports.connect = async (uri) => {
+  mongoose
+    .connect(uri)
+    .then(() => {
+      console.log("DB connection is successful...");
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
