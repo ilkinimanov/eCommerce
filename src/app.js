f6ae89a4df23f3d779b1e680b8dd9ab99f9fff55
ilkinimanov <coderilkin.03@gmail.com>
2023-06-07 15:21:15 +0400

    Set up NODE_ENV variable with cli flags Get cli flag --development or --production Assign NODE_ENV variable according to flag

--- src/app.js
+++ src/app.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
+const args = process.argv.slice(2);
 
 dotenv.config('../.env');
 
+if(args.includes('--development')) {
+  process.env.NODE_ENV = 'development';
+}
+else if(args.includes('--production')) {
+  process.env.NODE_ENV = 'production';
+}
+else {
+  console.log('Your NODE_ENV is not defined');
+  process.exit();
+}
 
 module.exports = app;
--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const server = require('./app.js');
 const PORT = process.env.PORT || 3000;
-
+const args = process.argv.slice(2);
 
 server.listen(PORT, () => {
   console.log("Server running...");
