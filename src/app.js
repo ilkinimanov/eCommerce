1e22db89d5e8a8686569b7f052ce78173cf8d60f
ilkinimanov <coderilkin.03@gmail.com>
2023-06-08 15:01:54 +0400

    Add morgan logger middleware - Morgan middleware added to app

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-const db = require('./db/db')
+const db = require('./db/db');
+const morgan = require('morgan');
 const dotenv = require('dotenv');
 const args = process.argv.slice(2);
 
@@ -20,10 +21,12 @@ else {
 if(process.env.NODE_ENV === 'development') {
   const uri = process.env.DEVELOPMENT_DB.replace('<password>', process.env.DEVELOPMENT_DB_PASSWORD)
   db.connect(uri);
+  app.use(morgan('dev'));
 }
 else if (process.env.NODE_ENV === 'production') {
   const uri = process.env.PRODUCTION_DB.replace('<password>', process.env.PRODUCTION_DB_PASSWORD)
   db.connect(uri);
+  app.use(morgan('combined'));
 }
 
 module.exports = app;
