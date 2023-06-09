d8a9a28e4da5e796366b6a7a22215a71907c5efe
ilkinimanov <coderilkin.03@gmail.com>
2023-06-09 18:32:54 +0400

    Create product controllers - Fundamental product CRUD controllers created

---
+++ src/controllers/productControllers.js
@@ -0,0 +1,87 @@
+const Product = require('../models/productModel');
+
+
+exports.getProducts = async(req, res) => {
+  try {
+    const products = await Product.find();
+    res
+      .status(200)
+      .json(products);
+  }
+  catch(err) {
+    res
+      .status(500)
+      .json({ message: err.message });
+  }
+};
+
+exports.createProduct = async(req, res) => {
+  try {
+    const newProduct = await Product.create(req.body);
+    res
+      .status(201)
+      .json(newProduct);
+  }
+  catch(err) {
+    res
+      .status(400)
+      .json({ message: err.message });
+  }
+};
+
+exports.getProductById = async(req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    if (!product) {
+      throw new Error('Product not found');
+    }
+    res
+      .status(200)
+      .json(product);
+  }
+  catch (err) {
+    res
+      .status(404)
+      .json({ message: err.message });
+  }
+};
+
+exports.updateProductById = async(req, res) => {
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {
+        new: false,
+        runValidators: true
+      });
+    if (!updatedProduct) {
+      throw new Error('Product not found');
+    }
+    res
+      .status(200)
+      .json(updatedProduct);
+  }
+  catch (err) {
+    res
+      .status(404)
+      .json({ message: err.message });
+  }
+};
+
+exports.deleteProduct = async(req, res) => {
+  try {
+    const product = await Product.findByIdAndDelete(req.params.id);
+    if (!product) {
+      throw new Error('Product not found');
+    }
+    res
+      .status(204)
+      .json({ status: "successful" })
+  }
+  catch (err) {
+    res
+      .status(404)
+      .json({ message: 'Product not found' });
+  }
+};
